42411e23d9a67668e61cb2e09d280af068c7d1db
DanielMagero <169893172+DanielMagero@users.noreply.github.com>
2024-12-13 00:12:47 +0300

    switching from sqlite to mysql

--- FirstProject/moviestore/moviestore/settings.py
+++ FirstProject/moviestore/moviestore/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'moviestore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'moviestore',
+        'USER': 'root',
+        'PASSWORD': 'daniel',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
---
+++ db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE moviestore;
+
+USE moviestore;
+
+SHOW TABLES;
