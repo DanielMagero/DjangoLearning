fbd819aa56946a3ff738a15f1526c468a9ee3dcd
DanielMagero <169893172+DanielMagero@users.noreply.github.com>
2024-12-15 19:40:01 +0300

    updating views to use calculation

--- FirstProject/moviestore/cart/views.py
+++ FirstProject/moviestore/cart/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404, redirect
 from movies.models import Movie
+from .utils import calculate_cart_total
 
 # Create your views here.
+
+def index(request):
+  cart_total  = 0
+  movies_in_cart = []
+  cart = request.session.get('cart', {})
+  movie_ids  = list(cart.keys())
+  if (movie_ids != []):
+      movies_in_cart = Movie.objects.filter(id__in = movie_ids )
+      cart_total = calculate_cart_total(cart, movies_in_cart)
 
 def add(request, id):
   get_object_or_404(Movie, id=id)
