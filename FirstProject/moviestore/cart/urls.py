1c58c1b4027ebbb51a24af2d5bc39b631c6d4f6c
DanielMagero <169893172+DanielMagero@users.noreply.github.com>
2024-12-16 22:09:23 +0300

    adding a cart index page

--- FirstProject/moviestore/cart/urls.py
+++ FirstProject/moviestore/cart/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name = 'cart.index'),
-  path('<int:id>/add', views.add, name = 'cart.add'),
+  path('<int:id>/add', views.add_to_cart, name = 'cart.add'),
 ]
